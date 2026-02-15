311676fbd8dafea5e21acfc2a4fdb6743e377e26
eco-5 <cdmeforplanet1@gmail.com>
2026-02-15 17:39:22 -0500

    Added intro
    Added a line to print a self-introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Catherine" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, my name is Catherine and I am a freshman at Hunter College High School!")
 
 if __name__ == "__main__":
     main()
